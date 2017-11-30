2855dae8875a4288ecb959513c9168356101e793
Voldie <voldiekami@gmail.com>
2017-11-30 10:04:22 +0100

    Relocated definition to their own source file and other minor changes.

--- jni/include/org_jhpm_helper.h
+++ jni/include/org_jhpm_helper.h
@@ -23,165 +23,28 @@
 #include<hpm/hpm.h>
 
 /**
- *	Get object float pointer.
- *	@Return get float array data type.
+ *  Forward function declaration.
  */
-HPM_ALWAYS_INLINE jfloatArray hpmjni_get_float_array_reference(
-        JNIEnv* __restrict__ env, jobject __restrict__ objref) {
-
-	/*	Check if object reference is valid. */
-	assert(objref);
-
-	/*	Get class type from object.	*/
-	jclass cs = (*env)->GetObjectClass(env, objref);
-	jfieldID field = (*env)->GetFieldID(env, cs, "e", "[F");
-
-	/*	Get float array reference object.	*/
-	jfloatArray arr = (*env)->GetObjectField(env, objref, field);
-
-	/*	return float array object.  */
-	return arr;
-}
-
-/**
- *	Get object float pointer.
- *	@Return get float array pointer data.
- */
-HPM_ALWAYS_INLINE jfloat* hpmjni_get_float_array_pointer_reference(
+extern jfloatArray hpmjni_get_float_array_reference(JNIEnv* __restrict__ env,
+        jobject __restrict__ objref);
+extern jfloat* hpmjni_get_float_array_pointer_reference(
         JNIEnv* __restrict__ env, jobject __restrict__ objref,
-        jfloatArray* __restrict__ array) {
-
-	/*	Requires non null object and array pointer.	*/
-	assert(objref && array);
-
-	/*	Fetch float array object.	*/
-	*array = hpmjni_get_float_array_reference(env, objref);
-
-	/*	Get class type from object.	*/
-	return (*env)->GetFloatArrayElements(env, *array, NULL);
-}
-
-/**
- *	Get object float pointer.
- *	@Return get float array pointer data.
- */
-HPM_ALWAYS_INLINE void hpmjni_get_float_array_pointer_reference_a_b(
+        jfloatArray* __restrict__ array);
+extern void hpmjni_get_float_array_pointer_reference_a_b(
         JNIEnv* __restrict__ env, jobject* __restrict__ objref,
-        jfloatArray** __restrict__ parray, jfloat** __restrict__ pfloat) {
-
-	const int n = 2;
-	int i;
-
-	/*	Requires non null object and array pointer.	*/
-	assert(objref && parray && pfloat);
-
-	/*	Get float pointer for each float array objects. */
-	for(i = 0; i < n; i++){
-		pfloat[i] = hpmjni_get_float_array_pointer_reference(env, objref[i], parray[i]);
-	}
-}
-
-/**
- *	Release
- */
-HPM_ALWAYS_INLINE void hpmjni_release_float_array_pointer_reference(
-        JNIEnv* __restrict__ env, const jfloatArray __restrict__ array,
-        const jfloat* __restrict__ fp) {
-
-	assert(array && fp);
-
-	(*env)->ReleaseFloatArrayElements(env, array, fp, 0);
-}
-
-/**
- *	Get object float pointer.
- *	@Return get float array pointer data.
- */
-HPM_ALWAYS_INLINE void hpmjni_get_float_array_pointer_reference_a_b_c(
+        jfloatArray* __restrict__ parray, jfloat** __restrict__ pfloat);
+extern void hpmjni_get_float_array_pointer_reference_a_b_c(
         JNIEnv* __restrict__ env, jobject* __restrict__ objref,
-        jfloatArray** __restrict__ parray, jfloat** __restrict__ pfloat) {
-
-	const int n = 3;
-	int i;
-
-	/*	Requires non null object and array pointer.	*/
-	assert(objref && parray && pfloat);
-
-	/*	Get float pointer for each float array objects. */
-	for(i = 0; i < n; i++){
-		pfloat[i] = hpmjni_get_float_array_pointer_reference(env, objref[i], parray[i]);
-	}
-}
-
-/**
- *	Get object float pointer.
- *	@Return get float array pointer data.
- */
-HPM_ALWAYS_INLINE void hpmjni_release_float_array_pointer_reference_a_b(
-        JNIEnv* __restrict__ env, jfloatArray** __restrict__ parray, jfloat** __restrict__ pfloat) {
-
-	const int n = 2;
-	int i;
-
-	/*	Requires non null object and array pointer.	*/
-	assert(parray && pfloat);
-
-	/*	Release all float array objects.    */
-	for(i = 0; i < n; i++){
-		(*env)->ReleaseFloatArrayElements(env, parray[i], pfloat[i], 0);
-	}
-}
-
-/**
- *	Release float pointer array.
- */
-HPM_ALWAYS_INLINE void hpmjni_release_float_array_pointer_reference_a_b_c(
+        jfloatArray** __restrict__ parray, jfloat** __restrict__ pfloat);
+extern void hpmjni_release_float_array_pointer_reference_a_b(
         JNIEnv* __restrict__ env, jfloatArray* __restrict__ parray,
-        jfloat** __restrict__ pfloat) {
-
-	const int n = 3;
-	int i;
-
-	/*	Requires non null object and array pointer.	*/
-	assert(parray && pfloat);
-
-	/*	Release each float array objects.    */
-	for(i = 0; i < n; i++){
-		(*env)->ReleaseFloatArrayElements(env, parray[i], pfloat[i], 0);
-	}
-}
-
-/**
- *	Create instance object of
- *	specified class type.
- *
- *	@Return
- */
-HPM_ALWAYS_INLINE jobject hpmjni_create_object_instance(
-        JNIEnv* __restrict__ env, jclass __restrict__ c) {
-
-	/*	Get constructor method ID.	*/
-	jmethodID constructor = (*env)->GetMethodID(env, c, "<init>", "()V");
-
-	/*	Create new object.	*/
-	jobject o = (*env)->NewObject(env, c, constructor);
-
-	/*	*/
-	return o;
-}
-
-/**
- *	Create clone.
- *
- *	@Return clone object.
- */
-HPM_ALWAYS_INLINE jobject hpmjni_create_clone(JNIEnv* __restrict__ env,
-        jobject __restrict__ o) {
-	jclass c = (*env)->GetObjectClass(env, o);
-
-	jmethodID mid = (*env)->GetMethodID(env, c, "<init>", "()V");
-
-	return (*env)->NewObject(env, c, mid, o);
-}
+        jfloat** __restrict__ pfloat);
+extern void hpmjni_release_float_array_pointer_reference_a_b_c(
+        JNIEnv* __restrict__ env, jfloatArray* __restrict__ parray,
+        jfloat** __restrict__ pfloat);
+extern jobject hpmjni_create_object_instance(JNIEnv* __restrict__ env,
+        jclass __restrict__ c);
+extern jobject hpmjni_create_clone(JNIEnv* __restrict__ env,
+        jobject __restrict__ o);
 
 #endif
